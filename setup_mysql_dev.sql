cf04846e53e695a9b10978b1a65dbb4fec5b5b91
Josh-dev69 <ebubejoshua69@gmail.com>
2023-11-24 03:26:26 +0100

    Updated

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,10 +1,14 @@
 -- Create the database if doesn't exit
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+
 -- Create new user
 CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+
 -- GRANT PREVILEGES ON hbnb_dev
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+
 -- Grant select privileges on performace schema to hbnb_dev
 GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
+
 -- Flush privileges to apply changes
 FLUSH PRIVILEGES;
