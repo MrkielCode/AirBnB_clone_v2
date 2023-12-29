b9d88063216645edc5a6df54f3fc70cda4fbd60b
MrkielCode <ikechukwu.ezekiel@gmail.com>
2023-12-29 16:07:59 +0100

    number route

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+"""
+This scripts displays Hello HBNB!
+
+"""
+from flask import Flask, url_for
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello():
+    """
+    function to display some string
+    """
+    return "Hello HBNB!"
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """
+    function to display Hbnb
+    """
+    return "HBNB"
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """ rounting to /c with parameter"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"C {text}"
+
+
+@app.route('/python/', strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text="is cool"):
+    """ routing to /python with parameter"""
+    if '_' in text:
+        text = text.replace('_', ' ')
+    return f"Python {text}"
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """ script to route if parameters is int"""
+    return f"{n} is a number"
+
+
+if __name__ == "__main__":
+    app.run()
